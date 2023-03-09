937efa730de79af8256e994f20509dab8ea28e88
Edwin <ed107107@gmail.com>
2023-03-10 00:00:25 +0300

    inittial commit

---
+++ features/step-definitions/customSteps.js
@@ -0,0 +1,12 @@
+const {Given, When, Then} = require('@cucumber/cucumber');
+Given('the numbers {int} and {int}', function (int, int2) {
+
+         });
+
+When('they are added', function () {
+
+});         
+
+Then('result is {int}', function (int) {
+
+         });
---
+++ features/test.feature
@@ -0,0 +1,10 @@
+Feature: Calc
+
+    Using BDD to perform calculations tests
+
+    Background: Operands
+        Given the numbers 12 and 6
+    
+    Example: Add
+        When they are added
+        Then result is 18
